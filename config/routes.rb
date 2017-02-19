ceba9f22dfd704ae14333597122f81a36193634b
Jordan Stapinski <jstapins@andrew.cmu.edu>
2017-02-19 11:33:27 -0500

    Fixed routes and added views and controllers for article

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,9 @@ KcwWorld::Application.routes.draw do
   resources :categories
   resources :proverbs
   resources :photos
+  resources :articles
     
   get 'home' => 'home#index', :as => :home
-  
-  match 'home' => 'home#index', :as => :home
   root :to => 'home#index'
 
 end
